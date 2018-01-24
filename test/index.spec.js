123d20a7ca182e8600c06970506a7c7f6ab70b67
David Clark <dclark@greenchef.com>
2018-01-24 15:12:30 -0700

    [ENG-125] Switched testing framework to mocha/sinon at request from Gregson.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,13 @@
-const serviceRequest = require('../index.js');
-const { uriOrUrlKey, getParsedUrl } = serviceRequest;
+const assert = require('assert');
+const sinon = require('sinon');
+
+const sr = require('../index.js');
+const { uriOrUrlKey, getParsedUrl } = sr;
 
 describe('uriOrUrlKey', function() {
   it('should return "url" for empty object', function() {
     const opts = {};
-    expect(uriOrUrlKey(opts)).toBe('url');
+    assert.deepStrictEqual(uriOrUrlKey(opts), 'url');
   });
 
   it('should prioritize "uri" over "url" if both props are present', function() {
@@ -12,47 +15,47 @@ describe('uriOrUrlKey', function() {
       uri: 'foo',
       url: 'foo',
     }
-    expect(uriOrUrlKey(opts)).toBe('uri');
+    assert.deepStrictEqual(uriOrUrlKey(opts), 'uri');
   });
 
   it('should return "url" if no "uri" prop is present', function() {
     const opts = {
       url: 'foo',
     }
-    expect(uriOrUrlKey(opts)).toBe('url');
+    assert.deepStrictEqual(uriOrUrlKey(opts), 'url');
   });
 });
 
 describe('getParsedUrl', function() {
   beforeEach(function() {
     this.opts = {
-      uri: 'https://testdoman/cache',
+      uri: 'https://user:pass@testdoman/cache',
     };
   });
 
   it('should parse a hostname prop', function() {
     const parsedUrl = getParsedUrl(this.opts);
-    expect(parsedUrl.hostname).toBeDefined();
+    assert(parsedUrl.hostname);
   });
 
   it('should parse a protocol prop', function() {
     const parsedUrl = getParsedUrl(this.opts);
-    expect(parsedUrl.protocol).toBeDefined();
+    assert(parsedUrl.protocol);
   });
 
-  it('should parse a auth prop', function() {
+  it('should parse an auth prop', function() {
     const parsedUrl = getParsedUrl(this.opts);
-    expect(parsedUrl.auth).toBeDefined();
+    assert(parsedUrl.auth);
   });
 
   it('should parse a path prop', function() {
     const parsedUrl = getParsedUrl(this.opts);
-    expect(parsedUrl.path).toBeDefined();
+    assert(parsedUrl.path);
   });
 
   it('should call uriOrUrlKey', function() {
-    const spy = spyOn(serviceRequest, 'uriOrUrlKey').and.callThrough();
+    const spy = sinon.spy(sr, 'uriOrUrlKey');
     const parsedUrl = getParsedUrl(this.opts);
-    expect(spy).toHaveBeenCalled();
+    assert(spy.calledOnce);
   });
 });
